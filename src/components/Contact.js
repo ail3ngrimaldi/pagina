45347b151af04d5a50496db4df4fd0f98755224c
ail3ngrimaldi <ailengrimaldi1@gmail.com>
2022-03-29 00:49:06 -0300

    Styles of form contact done, hover on button not working

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,25 +2,40 @@ import React from 'react';
 import '../styles/Contact.css';
 
 export default function Contact () {
-    return (
-        <div className='paragraph' id='Contact'>
-            <div className='Contact_p'>
-                <h2 className='Contact_h2'> Contact me</h2>
-                <p>For any question, suggestion or if you are looking a person for a trainee/Jr. position, feel free to send me an e-mail.</p>
-            </div>
-                <form>
-                    <div className='col'>
-                        <div className='form-group'>
-                            <label>First Name</label>
-                            <input type='text' />
-                        </div>
-                        <div>
-                            <label>Last Name</label>
-                            <input type='text' />
-                        </div>
-                    </div>
-                </form>
-            <button type='button' className='send_button'>Send</button>
+    return (     
+<div class="background">
+  <div class="container">
+    <div class="screen">
+      <div class="screen-body">
+        <div class="screen-body-item left">
+          <div class="app-title">
+            <span>Contact</span>
+            <span>me</span>
+          </div>
+          <div class="app-contact">CONTACT INFO : +54 11 3432 3195</div>
         </div>
+        <div class="screen-body-item">
+          <div class="app-form">
+            <div class="app-form-group">
+              <input class="app-form-control" placeholder="NAME"/>
+            </div>
+            <div class="app-form-group">
+              <input class="app-form-control" placeholder="EMAIL"/>
+            </div>
+            <div class="app-form-group">
+              <input class="app-form-control" placeholder="CONTACT NO"/>
+            </div>
+            <div class="app-form-group message">
+              <input class="app-form-control" placeholder="MESSAGE"/>
+            </div>
+            <div class="app-form-group buttons">
+              <button class="app-form-button">Send</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
     );
 }
