a2257666c93b89b986a47f151b96238e42c9a928
ail3ngrimaldi <ailengrimaldi1@gmail.com>
2022-03-31 23:50:50 -0300

    Contact js modified with methods for emailjs but not working, also not broken which is great bc I was expecting it to be broken but its not

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,8 +1,48 @@
 import '../styles/Contact.css';
 import SocialNetworkButtons from './SocialNetwork';
+import emailjs from 'emailjs-com';
+import Swal from 'sweetalert2';
+import { useState } from 'react';
+
+const SERVICE_ID = 'ACAVAMISERVICEID'
+const TEMPLATE_ID = 'ACAVAMITEMPLATEID'
+const USER_ID = 'ACAVAMIUSERID'
 
 export default function Contact () {
-    return (     
+
+  const [data, setData] = useState({
+    name:'',
+    email:'',
+    message:''
+  })
+
+  const handleOnSubmit = (e) => {
+    e.preventDefault();
+
+    setData({
+      ...data,
+      [e.target.name]:e.target.value
+    })
+
+    emailjs.sendForm(SERVICE_ID, TEMPLATE_ID, e.target, USER_ID)
+    .then((result) =>  { 
+      console.log(result.text);
+      Swal.fire({
+        icon: 'success',
+        title: 'Message sent successfully! I will be in touch soon'
+    })
+    }, (error) => {
+      console.log(error.text);
+      Swal.fire({
+        icon: 'error',
+        title: 'Ooops, something went wrong',
+        text: error.text
+      })
+    });
+    e.target.reset()
+  };
+  
+return (
 <div className="background">
   <div className="container">
     <div className="screen">
@@ -17,20 +57,38 @@ export default function Contact () {
           </div>
         </div>
         <div className="screen-body-item">
-          <div className="app-form">
+          <form className="app-form">
             <div className="app-form-group">
-              <input className="app-form-control" placeholder="Name"/>
+              <input 
+              className="app-form-control" 
+              placeholder="Name"
+              name='name'
+              required
+              />
             </div>
             <div className="app-form-group">
-              <input className="app-form-control" placeholder="Email"/>
+              <input 
+              className="app-form-control" 
+              placeholder="email"
+              name='email'
+              required
+              />
             </div>
             <div className="app-form-group message">
-              <input className="app-form-control" placeholder="Your message"/>
+              <input 
+              className="app-form-control" 
+              placeholder="Your message"
+              name='message'
+              required
+              />
             </div>
             <div className="app-form-group buttons">
-              <button className="app-form-button">Send</button>
+              <button 
+              className="app-form-button"
+              onSubmit={handleOnSubmit}
+              >Send</button>
             </div>
-          </div>
+          </form>
         </div>
       </div>
     </div>
