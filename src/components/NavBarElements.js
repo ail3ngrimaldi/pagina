cab2c2f8929442b04de216039145a988df9d3163
ail3ngrimaldi <ailengrimaldi1@gmail.com>
2022-04-06 00:41:12 -0300

    Contact button displays on every device

--- src/components/NavBarElements.js
+++ src/components/NavBarElements.js
@@ -44,25 +44,20 @@ export const NavMenu = styled.div`
   display: flex;
   align-items: center;
   margin-right: -24px;
-  /* Second Nav */
-  /* margin-right: 24px; */
-  /* Third Nav */
-  /* width: 100vw;
-  white-space: nowrap; */
   @media screen and (max-width: 768px) {
     display: none;
   }
 `;
 
+
 export const NavBtn = styled.nav`
   display: flex;
   align-items: center;
   margin-right: 24px;
-  /* Third Nav */
-  /* justify-content: flex-end;
-  width: 100vw; */
   @media screen and (max-width: 768px) {
-    display: none;
+    display: block;
+    margin-left: 24px;
+    margin-top: 24px;
   }
 `;
 
@@ -77,8 +72,6 @@ export const NavBtnLink = styled(Link)`
   cursor: pointer;
   transition: all 0.2s ease-in-out;
   text-decoration: none;
-  /* Second Nav */
-  margin-left: 24px;
   &:hover {
     transition: all 0.2s ease-in-out;
     background: #fff;
