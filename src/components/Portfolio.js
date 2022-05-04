74f4861b8829f3a3a1581109a02ea03ca5f41cbf
ail3ngrimaldi <ailengrimaldi1@gmail.com>
2022-05-03 23:17:42 -0300

    Card created

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,17 +1,20 @@
-import '../styles/Paragraph.css'
+import '../styles/Portfolio.css'
+import Card from './Card';
+import { data } from './data';
+
+console.log(data)
 
 function Portfolio () {
     return (
-        <div className='paragraph'>
-            <h2>Portfolio</h2>
-            <p>My firsts projects were developed as a student in 'Soy Henry', there's been a long way from that moment to my present, so there's an updated list:
-            </p>
-            <ul className='ul_portfolio'>
-                <li> An e-commerce for a candle store located in Córdoba, Argentina.</li>
-                <li> An e-commerce for a clothing store located in Buenos Aires, Argentina.</li>
-                <li> <a href='https://www.instagram.com/tachito.compost/?hl=es-la'>Tachito Compost</a>. A project in development. My job here is to develop an app with react-native that brings people closer to compost and ecology issues.</li>
-            </ul>
-        </div>
+        <div className='portfolio-container'>
+                <h2 className='portfolio-title'>Portfolio</h2>           
+                    <div className='cards-container'>
+                    {data.map(data => (
+                        <Card key={data.id} data={data} />
+                    ))}  
+                   
+                    </div>
+                    </div>
     )
 }
 
